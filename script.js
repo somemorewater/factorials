865729504bb2c88b475a743e61ff403fc84705d0
somemorewater <oboyiloejeh@gmail.com>
2025-01-11 05:48:20 +0100

    added all neccesry script for the permutation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const factorialInput = document.getElementById('factorialNumber');
+const permutationN = document.getElementById('permutationN');
+const permutationR = document.getElementById('permutationR');
 
 function error() {
     return 'E'
@@ -10,9 +12,33 @@ function factorial(n) {
     } else if (n == 0) {
         return 1;
     } else {
-        return n * factorial(n - 1);
+        const done = n * factorial(n - 1);
+        return done;
     }
 }
+
+function permutation(n, r) {
+    if (n >= 70 || r >= 70) {
+        return error();
+    } else if (r > n) {
+        return error();
+    } else if (n == 0 && r == 0) {
+        return 1;
+    } else if (n == 0 && r > 0) {
+        return error();
+    } else {
+        const done = factorial(n) / factorial(n - r);
+        return done;
+    }
+}
+
+document.querySelector('#permutationForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const permutationNum = permutationN.value;
+    const permutationReg = permutationR.value;
+    const result = permutation(permutationNum, permutationReg);
+    document.getElementById('permutationResult').innerHTML = result;
+});
 
 document.querySelector('#factorialForm').addEventListener('submit', (event) => {
   event.preventDefault();
