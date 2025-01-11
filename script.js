2341bb4c4ee8dd41cd52b6a61860c0b463ac53f4
somemorewater <oboyiloejeh@gmail.com>
2025-01-11 04:53:33 +0100

    made afew additions t the factorial function, added and error function

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const factorialInput = document.getElementById('factorialNumber');
 
+function error() {
+    return 'E'
+}
+
 function factorial(n) {
-    if (n == 0) {
+    if (n >= 70) {
+        return error();
+    } else if (n == 0) {
         return 1;
     } else {
         return n * factorial(n - 1);
     }
 }
 
-addEventListener('submit', (event) => {
+document.querySelector('#factorialForm').addEventListener('submit', (event) => {
   event.preventDefault();
   const factorialNumber = factorialInput.value;
   const result = factorial(factorialNumber);
